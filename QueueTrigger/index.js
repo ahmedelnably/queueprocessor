0286e927cb42bf7306054ec2e2d956cd5eec2fc9
Ahmed Elnably <aelnably@microsoft.com>
2019-08-14 14:47:24 -0700

    Update index.js

--- QueueTrigger/index.js
+++ QueueTrigger/index.js
@@ -1,5 +1,5 @@
 module.exports = async function (context, myQueueItem) {
     let retMsg = "Processing!"
-    context.log('New Version processed the message', myQueueItem);
+    context.log('Enhanced Version processed the message', myQueueItem);
     context.bindings.outputQueueItem = retMsg;
 };
